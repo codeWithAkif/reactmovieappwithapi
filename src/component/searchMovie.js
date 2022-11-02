c43d82fadbea3a3cefbee02ecf98fde35f3f29fa
codeWithAkif <104597749+codeWithAkif@users.noreply.github.com>
2022-11-02 17:14:20 +0300

    m

---
+++ src/component/searchMovie.js
@@ -0,0 +1,55 @@
+import React from "react";
+
+export default function searchMovie() {
+  return (
+    <div>
+      <div className="searchBar">
+        <div className="mainSearchBar">
+          <h2> Search Movie</h2>
+          <input type="text" placeholder="Search..." />
+        </div>
+      </div>
+
+      <div className="mainMovie">
+        <div className="mainBoxs">
+          <div className="boxs">
+            <img src="https://upload.wikimedia.org/wikipedia/en/7/7a/Center_of_the_earth_3d.jpg" />
+            <div className="movieInfo">
+              
+                <div className="MovieName">Journey to the center of the world</div> </div>
+          </div>
+          <div className="boxs">
+            <img src="https://upload.wikimedia.org/wikipedia/en/7/7a/Center_of_the_earth_3d.jpg" />
+            <div className="movieInfo">
+              
+                <div className="MovieName">Journey to the center of the world</div> </div>
+          </div>
+          <div className="boxs">
+            <img src="https://upload.wikimedia.org/wikipedia/en/7/7a/Center_of_the_earth_3d.jpg" />
+            <div className="movieInfo">
+              
+                <div className="MovieName">Journey to the center of the world</div> </div>
+          </div>
+          <div className="boxs">
+            <img src="https://upload.wikimedia.org/wikipedia/en/7/7a/Center_of_the_earth_3d.jpg" />
+            <div className="movieInfo">
+              
+                <div className="MovieName">Journey to the center of the world</div> </div>
+          </div>
+          <div className="boxs">
+            <img src="https://upload.wikimedia.org/wikipedia/en/7/7a/Center_of_the_earth_3d.jpg" />
+            <div className="movieInfo">
+              
+                <div className="MovieName">Journey to the center of the world</div> </div>
+          </div>
+          <div className="boxs">
+            <img src="https://upload.wikimedia.org/wikipedia/en/7/7a/Center_of_the_earth_3d.jpg" />
+            <div className="movieInfo">
+              
+                <div className="MovieName">Journey to the center of the world</div> </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
